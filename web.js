1ac46fadc11026a07313dd082d0bee0d989fcb11
Leonard Langsdorf <leonard.langsdorf@gmail.com>
2013-07-10 07:22:18 -0400

    Changes for question 1

--- web.js
+++ web.js
@@ -3,10 +3,17 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+
+  //response.send('Hello World!');
+  response.send(buffer.toString('utf8',0,buffer.length));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+var buffer = fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
 });
