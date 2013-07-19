3dbf104638bc6eec78472f368e07d5d00f112606
Leonard Langsdorf <leonard.langsdorf@gmail.com>
2013-07-19 19:12:36 -0400

    updated web.js
    should work more effeciently

--- web.js
+++ web.js
@@ -1,16 +1,16 @@
 var express = require('express');
-
+var fs = require('fs');
+var app = express.createServer(express.logger());
+var htmlfile = "index.html";
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
 
-  //response.send('Hello World!');
-  var fs = require('fs');
-  var buf = fs.readFileSync('index.html', 'utf8');
-  response.send(buf.toString('utf8',0,buf.length));
+app.get('/', function(request, response) {
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
